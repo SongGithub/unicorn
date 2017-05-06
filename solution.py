a994fb71bde512ed7a334412ebe7d0a3e4c598dd
SongJin@mbp15 <songjin@hotmail.com>
2017-05-06 13:08:13 +1000

    init

---
+++ solution.py
@@ -0,0 +1,31 @@
+from datetime import date
+
+
+def sort_list(l):
+    """Takes a list and returns a sorted version"""
+    return l
+
+
+def rgb_to_hex(red, green, blue):
+    """
+    Convert red, green, blue values into a HTML hex representation
+
+    The short syntax should (#fff) be used where possible.
+    """
+    return '#fff'
+
+
+def get_github_members(org_name):
+    """
+    Get the number of (public) members belonging to the specified Github
+    organisation
+    """
+    return 0
+
+
+def get_ssl_expiry(domain):
+    """
+    Takes a domain and returns a date that represents when the SSL certificate
+    will expire.
+    """
+    return date(2017, 1, 1)
